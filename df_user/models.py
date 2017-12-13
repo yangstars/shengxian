c1478d66da0e2e81cfe41eb69b340c3b82cde8b4
yangquanshuai <4692551@qq.com>
2017-12-13 16:36:01 +0800

    注册实现

--- df_user/models.py
+++ df_user/models.py
@@ -5,9 +5,9 @@ class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
     uemail = models.CharField(max_length=30)
-    ushou = models.CharField(max_length=30)
-    uaddress = models.CharField(max_length=100)
-    uyoubian=models.CharField(max_length=6)
-    uphone = models.CharField(max_length=11)
+    ushou = models.CharField(max_length=30,null=True)
+    uaddress = models.CharField(max_length=100,null=True)
+    uyoubian=models.CharField(max_length=6,null=True)
+    uphone = models.CharField(max_length=11,null=True)
 
 
--- df_user/views.py
+++ df_user/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+#-*- coding:utf-8 -*-
+from django.shortcuts import render ,redirect
+from models import *
+from hashlib import sha1 #引入加密模块
 
 # Create your views here.
+
+#跳转注册页面
+def register(request):
+    return render(request,'df_user/register.html')
+
+
+#跳转注册页面
+def login(request):
+    return render(request,'df_user/login.html')
+
+
+def register_handle(request):
+    post = request.POST
+    uname = post.get("user_name")
+    upwd = post.get("pwd")
+    cpwd = post.get("cpwd")
+    uemail = post.get("email")
+    #判断两次密码是否一致
+    if upwd != cpwd:
+        return  redirect('/user/register')
+
+    #密码加密
+    s1 = sha1()
+    s1.update(upwd)
+    upwd2= s1.hexdigest()
+
+    #创建对象
+    user = UserInfo()
+    user.uname = uname
+    user.upwd =upwd2
+    user.uemail = uemail
+    user.save()
+
+    #跳转到登陆页面
+    return redirect('/user/login')
+
+
+
+
+
+
+
+
+
+
